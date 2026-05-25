165991065e4f6bdf85dbf21bc145c3e8bc40e349
anilkishore1978 <109075470+anilkishore1978@users.noreply.github.com>
2026-05-25 13:19:05 +0530

    Add files via upload

--- ld-config.js
+++ ld-config.js
@@ -2,7 +2,7 @@ window.NACONFIG = {
   CLIENTID: 'd1141f25-2729-4ea5-9892-f80db4205ebf',
   TENANTID: '36090fda-8a83-4040-8c58-7cf784511505',
   TENANTSUB: 'birlasoftdentalentra',
-  PROXY: 'http://localhost:3002/proxy',
+  PROXY: 'https://fuzzy-fiesta-76rjr6g5x442pwj4-3002.app.github.dev/proxy',
   SCOPES: 'openid profile email',
   CHALTYPES: 'oob password redirect',
   pages: {
